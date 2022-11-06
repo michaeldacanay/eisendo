cd08b880bab51ac56e2703048b77ba9d9db73763
Michael Dacanay <mzdacana@ncsu.edu>
2022-11-06 00:21:13 -0400

    currently code is breaking, attempt to implement react navigation

--- App.js
+++ App.js
@@ -3,11 +3,15 @@ import { Keyboard, ScrollView, StyleSheet, Text, View, Image } from 'react-nativ
 import { styled } from 'nativewind';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 SplashScreen.preventAutoHideAsync();
+const Stack = createNativeStackNavigator();
 
 import { TaskInputField } from './components/TaskInputField.js';
 import { TaskItem } from './components/TaskItem.js';
+import TaskItemScreen from './components/TaskItemScreen.js';
 
 export default function App() {
   const [tasks, setTasks] = useState([])
@@ -36,23 +40,32 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      {/* <Text style={styles.heading}>eisendo</Text> */}
-      <Image source={require('./assets/icon.png')} style={styles.logo} />
-      <ScrollView style={styles.scrollView}>
-        {
-          tasks.map((task, index) => {
-            return (
-              <View key={index} style={styles.taskContainer}>
-                <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
-              </View>
-            );
-          })
-        }
-      </ScrollView>
-      {/* <PrioritizeButton></PrioritizeButton> */}
-      <TaskInputField addTask={addTask}/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <View style={styles.container} onLayout={onLayoutRootView}>
+          <Image source={require('./assets/icon.png')} style={styles.logo} />
+          
+          <Stack.Screen>
+            <ScrollView style={styles.scrollView}>
+              {
+                tasks.map((task, index) => {
+                  return (
+                    <View key={index} style={styles.taskContainer}>
+                      <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
+                    </View>
+                  );
+                })
+              }
+            </ScrollView>
+          </Stack.Screen>
+          
+
+          {/* <PrioritizeButton></PrioritizeButton> */}
+          <TaskInputField addTask={addTask}/>
+        </View>
+      </Stack.Navigator>
+      
+    </NavigationContainer>
   );
 }
 
