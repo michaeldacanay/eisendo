077f3c1f56b26ab0aafa30ae77d9062e4fb9f91e
Michael Dacanay <mzdacana@ncsu.edu>
2022-11-05 22:42:51 -0400

    Add fonts and logo, resolve splash screen issue

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import React, {useState} from 'react';
-import { Keyboard, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { Keyboard, ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 import { styled } from 'nativewind';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 import { TaskInputField } from './components/TaskInputField.js';
 import { TaskItem } from './components/TaskItem.js';
 
-const StyledView = styled(View)
-const StyledText = styled(Text)
 export default function App() {
   const [tasks, setTasks] = useState([])
+  const [ fontsLoaded ] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black-900.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   const addTask = (task) => {
     if (task == null) return;
@@ -21,8 +36,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>eisendo</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      {/* <Text style={styles.heading}>eisendo</Text> */}
+      <Image source={require('./assets/icon.png')} style={styles.logo} />
       <ScrollView style={styles.scrollView}>
         {
           tasks.map((task, index) => {
@@ -34,6 +50,7 @@ export default function App() {
           })
         }
       </ScrollView>
+      {/* <PrioritizeButton></PrioritizeButton> */}
       <TaskInputField addTask={addTask}/>
     </View>
   );
@@ -46,12 +63,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 100,
+    resizeMode: 'contain'
+  },
   heading: {
     color: '#fff',
     fontSize: 20,
     fontWeight: '600',
     marginTop: 50,
     marginBottom: 20,
+    fontFamily: 'Inter-Black',
   },
   scrollView: {
     marginBottom: 70,
